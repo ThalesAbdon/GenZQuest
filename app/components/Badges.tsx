0865af02d0952dc3009fcc1fe178bc40e26e4c94
ThalesAbdon <thales.sousufpi@gmail.com>
2023-12-27 15:29:14 -0300

    chore: Environment setup

---
+++ app/components/Badges.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { FaMedal } from "react-icons/fa6";
+import { AiOutlinePlus} from "react-icons/ai"
+
+const Badges = () => {
+    const onClick = () => {
+
+    }
+    return ( 
+        <div className="flex flex-col">
+            <div className="
+                flex
+                items-center
+                justify-between
+                px-5
+                pt-4
+            ">
+            <div className="
+                inline-flex
+                items-center
+                gap-x-2
+            "
+            >
+                <FaMedal className="text-neutral-400" size={26}/>
+                <p
+                    className="
+                        text-neutral-400
+                        font-medium
+                        text-md
+                    "
+                >
+                    Suas Conquistas!
+                </p>
+            </div>    
+            <AiOutlinePlus
+                onClick = {onClick}
+                size={20}
+                className="
+                    text-neutral-400
+                    cursor-pointer
+                    hover:text-white
+                    transition
+                "
+            />
+            </div>
+            <div className="
+                flex
+                flex-col
+                gap-y-2
+                mt-4
+                px-3
+            ">
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default Badges;
